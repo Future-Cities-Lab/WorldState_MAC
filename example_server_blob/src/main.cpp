87f56a16e99072c26aab8745680617938f972439
Collin  Schupman <collinschupman@Collins-MacBook-Pro.local>
2016-11-03 11:47:53 -0700

    final mac update

--- example_server_blob/src/main.cpp
+++ example_server_blob/src/main.cpp
@@ -3,11 +3,8 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(72, 24, OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+	//ofSetupOpenGL(80, 24, OF_WINDOW);
+    ofSetupOpenGL(80, 48, OF_WINDOW);
 	ofRunApp( new ofApp());
 
 }
--- example_server_blob/src/ofApp.cpp
+++ example_server_blob/src/ofApp.cpp
@@ -1,6 +1,7 @@
 #include "ofApp.h"
 #include "ofxCrypto.h"
 #include "ofxUDPManager.h"
+#include <typeinfo>
 
 #ifdef TARGET_OSX
 #include <ifaddrs.h>
@@ -9,21 +10,37 @@
 #include <arpa/inet.h>
 #endif
 
-ofImage testImage;
-ofBuffer decodedImage;
+/*
+ TODO: Test 
+ See if we can reasonably open up to sockets.
+ 
+ */
+
+ofImage testImage1;
+ofImage testImage2;
+
+ofBuffer decodedImage1;
+ofBuffer decodedImage2;
 
 ofxUDPManager udpConnectionBroadcast;
-ofxUDPManager moduleConnections[6];
 
-char pixelsToSend[6][864];
-char pixelsToSendTest[864];
+ofxUDPManager moduleConnectionsZone1[6];
+ofxUDPManager moduleConnectionsZone2[7];
+
+
+char pixelsToSendZone1[6][864];
+char pixelsToSendZone2[7][864];
 
 int cosValue = 0;
 
-const static int totalHeight = 24;
-const static int totalWidth_72 = 72;
+const static int totalHeight_24 = 24;
+
+const static int totalWidth_68 = 68;
+const static int totalWidth_80 = 80;
 
 ofVideoPlayer myPlayer;
+ofVideoPlayer myPlayerZone2;
+
 
 int mode = 4;
 
@@ -41,9 +58,6 @@ void ofApp::setup(){
     messages.push_back("WebSocket server setup at "+ofToString( server.getPort() ) + ( server.usingSSL() ? " with SSL" : " without SSL") );
     locked = needToLoad = false;
     
-    for (int i = 0; i < 864; i++) {
-        pixelsToSendTest[i] = '\0';
-    }
     
     /* Module Communication */
     udpConnectionBroadcast.Create();
@@ -54,216 +68,154 @@ void ofApp::setup(){
     std::string messageSent = "**";
     udpConnectionBroadcast.Send(messageSent.c_str(), messageSent.size());
     
-    // WAIT
     ofSleepMillis(100);
+    
     char udpMessage[1000];
     auto ret = udpConnectionBroadcast.Receive(udpMessage, 1000);
     while (udpMessage[0] != '\0') {
-        cout << ((int)udpMessage[0]) - 101 << endl;
-        ofxUDPManager udpConnectionRx;
-        moduleConnections[((int)udpMessage[0]) - 101] = udpConnectionRx;
-        moduleConnections[((int)udpMessage[0]) - 101].Create();
-        moduleConnections[((int)udpMessage[0]) - 101].SetNonBlocking(true);
-        moduleConnections[((int)udpMessage[0]) - 101].Bind(6000);
-        string ip = "192.168.2." + ofToString((int)udpMessage[0]);
-        cout << ip << endl;
-        moduleConnections[((int)udpMessage[0]) - 101].Connect(ip.c_str(), 8888);
-        moduleConnections[((int)udpMessage[0]) - 101].SetEnableBroadcast(false);
-        std::fill_n(udpMessage, 1000, 0);
-        auto ret = udpConnectionBroadcast.Receive(udpMessage, 1000);
+        int num = ((int)udpMessage[0]) - 101;
+        if (num < 7) {
+            ofxUDPManager udpConnectionRx;
+            moduleConnectionsZone2[((int)udpMessage[0]) - 101] = udpConnectionRx;
+            moduleConnectionsZone2[((int)udpMessage[0]) - 101].Create();
+            moduleConnectionsZone2[((int)udpMessage[0]) - 101].SetNonBlocking(true);
+            moduleConnectionsZone2[((int)udpMessage[0]) - 101].Bind(6000);
+            string ip = "192.168.2." + ofToString((int)udpMessage[0]);
+            moduleConnectionsZone2[((int)udpMessage[0]) - 101].Connect(ip.c_str(), 8888);
+            moduleConnectionsZone2[((int)udpMessage[0]) - 101].SetEnableBroadcast(false);
+            std::fill_n(udpMessage, 1000, 0);
+            auto ret = udpConnectionBroadcast.Receive(udpMessage, 1000);
+        } else {
+            ofxUDPManager udpConnectionRx;
+            moduleConnectionsZone1[((int)udpMessage[0]) - 108] = udpConnectionRx;
+            moduleConnectionsZone1[((int)udpMessage[0]) - 108].Create();
+            moduleConnectionsZone1[((int)udpMessage[0]) - 108].SetNonBlocking(true);
+            moduleConnectionsZone1[((int)udpMessage[0]) - 108].Bind(6000);
+            string ip = "192.168.2." + ofToString((int)udpMessage[0]);
+            cout << ip << endl;
+            moduleConnectionsZone1[((int)udpMessage[0]) - 108].Connect(ip.c_str(), 8888);
+            moduleConnectionsZone1[((int)udpMessage[0]) - 108].SetEnableBroadcast(false);
+            std::fill_n(udpMessage, 1000, 0);
+            auto ret = udpConnectionBroadcast.Receive(udpMessage, 1000);
+        }
+        cout << "" << endl;
     }
     ofSleepMillis(1000);
-    testImage.load("rainbow_texture2.jpg");
-    ofPixels pix;
-    pix.allocate(72, 24, 3);
-    text.allocate(pix);
-    myPlayer.loadMovie("KanyeMode.mp4");
-    myPlayer.play();
+    
+    testImage1.load("texture.png");
+    testImage2.load("texture2.png");
+
 }
 
 void ofApp::update(){
-    
-    myPlayer.update(); // get all the new frames
-    
-    if ( needToLoad ){
-        testImage.load(decodedImage);
+    if ( needToLoad ) {
+        testImage1.load(decodedImage1);
+        testImage2.load(decodedImage2);
         needToLoad = false;
         locked = false;
     }
-    
-    for (int i = 0; i < 864; i++) {
-        int level = pixelsToSendTest[i];
-        level--;
-        level %= 255;
-        pixelsToSendTest[i] = level;
-    }
-
-    cosValue++;
-
-    if (mode == 0 || mode == 2) {
-        cosValue%=24;
-    } else if (mode == 4) {
-        cosValue%=72;
-    } else if (mode == 3) {
-        if (cosValue >= 72) {
-            cosValue = 60;
-        }
-    }
-
-   //ofSleepMillis(10);
 }
 
 void ofApp::draw() {
     ofSetBackgroundColor(0.0);
     
-  // SIN WAVE2
-    ofPixels pix;
+    ofPixels pix1;
 
-    if (mode == 0) {
-        for (int i = 0; i < 24; i++) {
-            float co = ofMap(cosValue, 0, 24, 0.0, 2*PI);
-            cosValue++;
-            cosValue%=24;
-            ofSetColor(ofMap(cos(co), -1.0, 1.0, 0, 255.0));
-            ofFill();
-            ofDrawLine(0, i, 74, i);
-        }
-        text.loadScreenData(0, 0, 72, 24);
-        text.readToPixels(pix);
-    } else if (mode == 1 || mode == 2 || mode == 3) {
-        text.loadScreenData(0, 0, 72, 24);
-        text.readToPixels(pix);
-    } else if (mode == 4) {
-        for (int i = 0; i < 72; i++) {
-            float co = ofMap(cosValue, 0, 72, 0.0, 2*PI);
-            cosValue++;
-            cosValue%=72;
-            ofSetColor(ofMap(cos(co), -1.0, 1.0, 0, 255.0));
-            ofFill();
-            ofDrawLine(i, 0, i, 24);
-        }
-        text.loadScreenData(0, 0, 72, 24);
-        text.readToPixels(pix);
-    } else if (mode == 5) {
-        if ( testImage.isAllocated() ) {
-            testImage.draw(0, 0);
-        }
-    }
-
-    ofPixels pixels;
-    
-    if (mode == 0 || mode == 4) {
-        pixels = pix;
-    } else if (mode == 2) {
-        pixels = pix;
-        for (int j = 0; j < 72; j++) {
-            pixels[3*(j + cosValue*72)+0] = 255;
-            pixels[3*(j + cosValue*72)+1] = 255;
-            pixels[3*(j + cosValue*72)+2] = 255;
-        }
-    } else if (mode == 1) {
-        myPlayer.draw(0, 0);
-        if (myPlayer.isFrameNew()){
-            pixels = myPlayer.getPixels();
-        }
-    } else if (mode == 3) {
-        pixels = pix;
-        for (int j = 0; j < 24; j++) {
-            pixels[3*(cosValue + j*72)+0] = 255;
-            pixels[3*(cosValue + j*72)+1] = 255;
-            pixels[3*(cosValue + j*72)+2] = 255;
-        }
-    } else if (mode == 5) {
-        if ( testImage.isAllocated() ) {
-            pixels = testImage.getPixels();
-        }
-    }
-
-    if (myPlayer.isFrameNew() || mode != 1 || mode != 5) {
+    if ( testImage1.isAllocated() ) {
+        ofPixels pix;
+        testImage1.draw(0, 0);
+        pix = testImage1.getPixels();
         for (int i = 0; i < 288; i++) {
-            pixelsToSend[0][3*i] = pixels[panel1[i]+0];
-            pixelsToSend[0][3*i+1] = pixels[panel1[i]+1];
-            pixelsToSend[0][3*i+2] = pixels[panel1[i]+2];
+            pixelsToSendZone1[0][3*i] = pix[panel1Z1[i]+0];
+            pixelsToSendZone1[0][3*i+1] = pix[panel1Z1[i]+1];
+            pixelsToSendZone1[0][3*i+2] = pix[panel1Z1[i]+2];
         }
         for (int i = 0; i < 288; i++) {
-            pixelsToSend[1][3*i] = pixels[panel2[i]+0];
-            pixelsToSend[1][3*i+1] = pixels[panel2[i]+1];
-            pixelsToSend[1][3*i+2] = pixels[panel2[i]+2];
+            pixelsToSendZone1[1][3*i] = pix[panel2Z1[i]+0];
+            pixelsToSendZone1[1][3*i+1] = pix[panel2Z1[i]+1];
+            pixelsToSendZone1[1][3*i+2] = pix[panel2Z1[i]+2];
 
         }
         for (int i = 0; i < 288; i++) {
-            pixelsToSend[2][3*i] = pixels[panel3[i]+0];
-            pixelsToSend[2][3*i+1] = pixels[panel3[i]+1];
-            pixelsToSend[2][3*i+2] = pixels[panel3[i]+2];
+            pixelsToSendZone1[2][3*i] = pix[panel3Z1[i]+0];
+            pixelsToSendZone1[2][3*i+1] = pix[panel3Z1[i]+1];
+            pixelsToSendZone1[2][3*i+2] = pix[panel3Z1[i]+2];
 
         }
         for (int i = 0; i < 288; i++) {
-            pixelsToSend[3][3*i] = pixels[panel4[i]+0];
-            pixelsToSend[3][3*i+1] = pixels[panel4[i]+1];
-            pixelsToSend[3][3*i+2] = pixels[panel4[i]+2];
+            pixelsToSendZone1[3][3*i] = pix[panel4Z1[i]+0];
+            pixelsToSendZone1[3][3*i+1] = pix[panel4Z1[i]+1];
+            pixelsToSendZone1[3][3*i+2] = pix[panel4Z1[i]+2];
+        }
+        for (int i = 0; i < 288; i++) {
+            pixelsToSendZone1[4][3*i] = pix[panel5Z1[i]+0];
+            pixelsToSendZone1[4][3*i+1] = pix[panel5Z1[i]+1];
+            pixelsToSendZone1[4][3*i+2] = pix[panel5Z1[i]+2];
 
         }
         for (int i = 0; i < 288; i++) {
-
-            pixelsToSend[4][3*i] = pixels[panel5[i]+0];
-            pixelsToSend[4][3*i+1] = pixels[panel5[i]+1];
-            pixelsToSend[4][3*i+2] = pixels[panel5[i]+2];
-
-        }
-        for (int i = 0; i < 288; i++) {
-            pixelsToSend[5][3*i] = pixels[panel6[i]+0];
-            pixelsToSend[5][3*i+1] = pixels[panel6[i]+1];
-            pixelsToSend[5][3*i+2] = pixels[panel6[i]+2];
+            pixelsToSendZone1[5][3*i] = pix[panel6Z1[i]+0];
+            pixelsToSendZone1[5][3*i+1] = pix[panel6Z1[i]+1];
+            pixelsToSendZone1[5][3*i+2] = pix[panel6Z1[i]+2];
         }
         for (int panel = 0; panel < 6; panel++) {
-            moduleConnections[panel].Send(pixelsToSend[panel], 864);
+            moduleConnectionsZone1[panel].Send(pixelsToSendZone1[panel], 864);
             ofSleepMillis(1);
         }
         for (int panel = 0; panel < 6; panel++) {
-            moduleConnections[panel].Send("*", 1);
+            moduleConnectionsZone1[panel].Send("*", 1);
         }
-    } else if (mode == 5 && testImage.isAllocated() ) {
+    }
+    if ( testImage2.isAllocated()) {
+        ofPixels pix;
+        testImage2.draw(0, 24);
+        pix = testImage2.getPixels();
         for (int i = 0; i < 288; i++) {
-            pixelsToSend[0][3*i] = pixels[panel1[i]+0];
-            pixelsToSend[0][3*i+1] = pixels[panel1[i]+1];
-            pixelsToSend[0][3*i+2] = pixels[panel1[i]+2];
-        }
-        for (int i = 0; i < 288; i++) {
-            pixelsToSend[1][3*i] = pixels[panel2[i]+0];
-            pixelsToSend[1][3*i+1] = pixels[panel2[i]+1];
-            pixelsToSend[1][3*i+2] = pixels[panel2[i]+2];
-            
+            pixelsToSendZone2[0][3*i] = pix[panel1Z2[i]+0];
+            pixelsToSendZone2[0][3*i+1] = pix[panel1Z2[i]+1];
+            pixelsToSendZone2[0][3*i+2] = pix[panel1Z2[i]+2];
         }
         for (int i = 0; i < 288; i++) {
-            pixelsToSend[2][3*i] = pixels[panel3[i]+0];
-            pixelsToSend[2][3*i+1] = pixels[panel3[i]+1];
-            pixelsToSend[2][3*i+2] = pixels[panel3[i]+2];
-            
+            pixelsToSendZone2[1][3*i] = pix[panel2Z2[i]+0];
+            pixelsToSendZone2[1][3*i+1] = pix[panel2Z2[i]+1];
+            pixelsToSendZone2[1][3*i+2] = pix[panel2Z2[i]+2];
+
         }
         for (int i = 0; i < 288; i++) {
-            pixelsToSend[3][3*i] = pixels[panel4[i]+0];
-            pixelsToSend[3][3*i+1] = pixels[panel4[i]+1];
-            pixelsToSend[3][3*i+2] = pixels[panel4[i]+2];
-            
+            pixelsToSendZone2[2][3*i] = pix[panel3Z2[i]+0];
+            pixelsToSendZone2[2][3*i+1] = pix[panel3Z2[i]+1];
+            pixelsToSendZone2[2][3*i+2] = pix[panel3Z2[i]+2];
+
         }
         for (int i = 0; i < 288; i++) {
-            
-            pixelsToSend[4][3*i] = pixels[panel5[i]+0];
-            pixelsToSend[4][3*i+1] = pixels[panel5[i]+1];
-            pixelsToSend[4][3*i+2] = pixels[panel5[i]+2];
-            
+            pixelsToSendZone2[3][3*i] = pix[panel4Z2[i]+0];
+            pixelsToSendZone2[3][3*i+1] = pix[panel4Z2[i]+1];
+            pixelsToSendZone2[3][3*i+2] = pix[panel4Z2[i]+2];
+
         }
         for (int i = 0; i < 288; i++) {
-            pixelsToSend[5][3*i] = pixels[panel6[i]+0];
-            pixelsToSend[5][3*i+1] = pixels[panel6[i]+1];
-            pixelsToSend[5][3*i+2] = pixels[panel6[i]+2];
+
+            pixelsToSendZone2[4][3*i] = pix[panel5Z2[i]+0];
+            pixelsToSendZone2[4][3*i+1] = pix[panel5Z2[i]+1];
+            pixelsToSendZone2[4][3*i+2] = pix[panel5Z2[i]+2];
+
         }
-        for (int panel = 0; panel < 6; panel++) {
-            moduleConnections[panel].Send(pixelsToSend[panel], 864);
+        for (int i = 0; i < 288; i++) {
+            pixelsToSendZone2[5][3*i] = pix[panel6Z2[i]+0];
+            pixelsToSendZone2[5][3*i+1] = pix[panel6Z2[i]+1];
+            pixelsToSendZone2[5][3*i+2] = pix[panel6Z2[i]+2];
+        }
+        for (int i = 0; i < 288; i++) {
+            pixelsToSendZone2[6][3*i] = pix[panel7Z2[i]+0];
+            pixelsToSendZone2[6][3*i+1] = pix[panel7Z2[i]+1];
+            pixelsToSendZone2[6][3*i+2] = pix[panel7Z2[i]+2];
+        }
+        for (int panel = 0; panel < 7; panel++) {
+            moduleConnectionsZone2[panel].Send(pixelsToSendZone2[panel], 864);
             ofSleepMillis(1);
         }
-        for (int panel = 0; panel < 6; panel++) {
-            moduleConnections[panel].Send("*", 1);
+        for (int panel = 0; panel < 7; panel++) {
+            moduleConnectionsZone2[panel].Send("*", 1);
         }
     }
 }
@@ -288,13 +240,13 @@ void ofApp::onIdle( ofxLibwebsockets::Event& args ) {
 
 void ofApp::onMessage( ofxLibwebsockets::Event& args ) {
     if ( !args.isBinary && !locked ) {
-        decodedImage.clear();
-        decodedImage = ofxCrypto::base64_decode_to_buffer(args.message);
+        decodedImage1.clear();
+        decodedImage2.clear();
+        decodedImage1 = ofxCrypto::base64_decode_to_buffer(ofSplitString(args.message, "STARTNEWIMAGE")[0]);
+        decodedImage2 = ofxCrypto::base64_decode_to_buffer(ofSplitString(args.message, "STARTNEWIMAGE")[1]);
         needToLoad = true;
         locked = true;
     } else if (args.isBinary && !locked ) {
-        decodedImage.clear();
-        decodedImage.set(args.data.getData(), args.data.size());
         locked = true;
         needToLoad = true;
     }
